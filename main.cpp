bf9710a312314725d01d781375dd2e74a5d2d3c6
Pikka2048 <0312pika0201@gmail.com>
2022-07-23 14:01:45 +0900

    Add Test Tree Data

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@ int main()
 {
     node_t *root = MakeRootNode("Hack");
     std::string root_str = root->data;
-    std::cout << "Root is" << root_str << std::endl;
+    std::cout << "Root is " << root_str << std::endl;
 
-    node_t *result;
-    result = AddNode(root,"Apple");
-    std::string result_str = result->data;
-    std::cout << "Child is" << result_str << std::endl;
+    node_t *child;
+    child = AddNode(root, "Apple");
+    std::string child_str = child->data;
+    std::cout << "Child is " << child_str << std::endl;
+
+    node_t *grandchild;
+    grandchild = AddNode(child, "Grepe");
+    std::string grandchild_str = grandchild->data;
+    std::cout << "Child is " << grandchild_str << std::endl;
 }
