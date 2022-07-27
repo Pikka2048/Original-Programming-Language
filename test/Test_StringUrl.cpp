acecd74529cdcd6370f2cffe3d828fec708f1b62
Pikka2048 <0312pika0201@gmail.com>
2022-07-27 12:39:33 +0900

    Addded StringUtl Test Code

--- test/Test_StringUrl.cpp
+++ test/Test_StringUrl.cpp
@@ -1,11 +1,20 @@
 #include "CppUTest/CommandLineTestRunner.h"
+#include "../scr/StringUtl.h"
+#include <vector>
+#include <string>
 
-TEST_GROUP(TestFuncInc){};
+TEST_GROUP(TestSplit1){};
 
-TEST(TestFuncInc, CheckReturnValue)
+TEST(TestSplit1, CheckReturnValue)
 {
-      LONGS_EQUAL(11, 11);
+    std::vector<std::string> result;
+    std::vector<std::string> delims = {"!", "?", "@", "#,", "$"};
+    std::string test_string = "Don't?get#in@my$way,!";
+    result = split(test_string, delims);
+    int answer = 6;
+    LONGS_EQUAL(6, answer);
 }
-int main(int argc, char **argv){
-      return CommandLineTestRunner::RunAllTests(argc, argv);
+int main(int argc, char **argv)
+{
+    return CommandLineTestRunner::RunAllTests(argc, argv);
 }
