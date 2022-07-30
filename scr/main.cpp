0cb7c00d3243f55bb0b212fed95533699b330cbe
Pikka2048 <0312pika0201@gmail.com>
2022-07-30 21:26:48 +0900

    Delete Old Example

--- scr/main.cpp
+++ scr/main.cpp
@@ -4,36 +4,6 @@
 #include "./utility/StringUtl.h"
 #include <chrono>
 
-void Example()
-{
-    node_t *root = MakeRootNode();
-    std::string root_str = root->data;
-    std::cout << "Root is " << root_str << std::endl;
-
-    node_t *child;
-    child = AddNode(root, "Apple");
-    std::string child_str = child->data;
-    std::cout << "Child is " << child_str << std::endl;
-
-    node_t *grandchild;
-    grandchild = AddNode(child, "Grepe");
-    std::string grandchild_str = grandchild->data;
-    std::cout << "Child is " << grandchild_str << std::endl;
-
-    DebugString("---------------------------");
-    DisplayNodeConsole(root);
-    DebugString("---------------------------");
-    auto readfile = LoadTextFile(".././program.hoge");
-    Parse(readfile);
-    DebugString("---------------------------");
-    std::vector<std::string> delims = {"(", ")", R"(")"};
-    ;
-    std::vector<std::string> result = split(R"(print("Hello My Lang"))", delims);
-    for (std::string x : result)
-    {
-        std::cout << x << "\n";
-    }
-}
 void BenchmarkSplit(int num, std::string str)
 {
     std::vector<std::string> delims = {"!", "?", "@", "#,", "$"};
